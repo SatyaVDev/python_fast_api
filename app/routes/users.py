a24e413aee1506787d8e00688e42990ef55ea73a
Satish Vishwakarma <staya9125@gmail.com>
2025-06-16 22:11:38 +0530

    setup fast api

---
+++ app/routes/users.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/users")
+def get_users():
+    return {"message": "List of users"}
+
+
+@router.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"message": f"User with ID {user_id}"}
